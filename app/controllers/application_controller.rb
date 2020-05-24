1df2a617f0134b46f2afbfa9455ef916f98a698c
kai815 <h.kaizuka815@gmail.com>
2020-05-24 14:55:36 +0900

    ログイン・ログアウト機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  private
+
+  # ログインしているユーザーを取得処理をコントローラやビューから呼び出しやすくする
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
