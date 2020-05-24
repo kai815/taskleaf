c54634bf874c3bb81548ade9b297d5949e8ef0a3
kai815 <h.kaizuka815@gmail.com>
2020-05-24 09:59:17 +0900

    名称にバリデーションの追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,11 @@
 class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: {maximum: 30}
+  validate :validates_name_not_including_comma
+
+  private
+
+  def validates_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
