93155beef8e7fec167a9f4fdb03750e3a9cfe777
kai815 <h.kaizuka815@gmail.com>
2020-05-25 15:43:07 +0900

    新規作成時の確認画面の追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,10 +13,21 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
+  end
+
   def create
     @task = current_user.tasks.new(task_params)
     # 下記のようにもかける
     # @task = Task.new(task_params.merge(user_id: current_user.id))
+
+    #確認画面で戻るボタンが押されたかどうかのチェック
+    if params[:back].present?
+      render :new
+      return
+    end
     if @task.save
       # ログの出力
       # logger.debug "task: #{@task.attributes.inspect}"
